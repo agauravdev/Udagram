a03c4bd460a1a91de0a811edfefb4f3fac16a705
Gaurav Agarwal <gauravggg21@gmail.com>
2020-04-22 13:36:21 +0530

    Added filterimage endpoint and tested to work locally

--- src/server.ts
+++ src/server.ts
@@ -13,24 +13,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async (req,res)=> {
+  	let {image_url} = req.query;
+  	if(!image_url){
+  		res.status(400).send('Error : Url of image not sent');
+  	}
+  	else{
+  		try{
+  			const result = await filterImageFromURL(image_url);
+  			res.sendFile(result, ()=> {deleteLocalFiles([result]);});
+  		}
+  		catch(err){
+  			res.status(422).send("Unprocessable Entity");
+  		}
+  		
 
-  /**************************************************************************** */
+  	}
+  });
 
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
